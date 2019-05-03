897e6e8b7ddffdee5067099718c0079fc1678885
Tobias Sailer <tobias.sailer@gmail.com>
2019-05-03 23:11:42 +0200

    Better Log

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles.css";
 import polyline from "@mapbox/polyline";
+import util from "util";
+
 // returns an array of lat, lon pairs
 // "msydHgx`q@_A~IkDL\\pJsPrWcQhv@cJxKq@bIpIjAzE~OzKdMbKrd@jK~FtMbTbL}H~DzVpLnJpTwB_CpPdNjOsDkKoNiGiKdHcIiDoTnDm]gMwWlQgy@sOiQpUyQqTuZdHwHgBlHuCqLoCyIzBaBpGgHSyKwF{AgIuGcAsNdIc]v@cKqEyLxFoYuMkd@pGoYsFkI`FuEpRy_@|g@iLlp@gIvBcUyRbAyJ_KEeV_]gLgGiNe}@yFmM|J}LeJiDoAoH{Uh@}BgT{Hk@qLcOja@aZsPyXgDyOzKLt@kN_ScXoAuTp@oYxN}LxQ}CgNm_AjJuCtj@cn@bh@sI~C{NnWmEjCiIhHWpDqHde@A~X_Nv]hO`S`Sda@aGfL`LGxEwLdQ`N~e@iT`k@XhEdDaBkC|Z~Jr_@}Sp_@~D`ReFg@qEjLiTsNeAwG}Ht@{BhFGt]lJtOxJfE`D|J`R|Hle@kTzPrY~ZmLlPpEbMqGxJeQxYqM~\\zNb\\uE|PlX~DoIlGnArGaHbQmw@nHcM"
 //  "yo`bHy}{v@vC}PhIvFnFcPpQnAjVnXvPmV|v@jMhPnfBnMmJz[zQgFjl@vLu@xg@s}@hJp@jYee@lH~UfGz@FaZrGgL`fAdrAnk@lInTlW~XjDtS|zBvk@}@xE|hAcItp@dPnyAfPlg@?f^nHLpBdUly@d_@iDzPhKjvD|BdPtJhHoHvmBnBtaBtOtuBoBn\\v\\|fC|GfpBfMtw@`jA|wBjeAlOnWwHL}}@fPgPbFwZru@eWb}@k~AzQeK}CcH~Rcj@xbAaqBbk@wuCpYgz@dd@wc@~e@qtAnH}g@dYiQba@vFbTwv@hF{hAmH[oA_YkIyJwJ_d@}YqUnYrUxInb@jJ~@~@yKoClKuGiAiKgd@cZ{TwEgW}\\qi@wJuj@_k@k_@zA}ScQzAmg@sb@}YrJaIj}@nIvo@mBrDoFyQ_OGoKvYaOn{EgGv[qDMeMmKyIyf@m\\ea@gr@o]e_@wg@_`@hBwO~\\s^DaBg_@ma@eyAeLySsRw@pM{Y{G{IpF_PrOuaCieAk_@yK_WvHiq@aHcDUga@yCjY}Ca`@a]ub@qPiBuMna@}\\wB`Hpu@oS`ZiNlf@yXmp@mEbNkY~CuBqZ}V}F?}]{Ymx@yi@oGmTyVid@wHaG@bG|SczAz^kGfPe]uRyGpg@qMtQaFkUrBo[e\\jL}i@qUsCydAmQggBcw@wLgQ~UyYe\\gNv@oElPoIcGoCfP"
@@ -48,4 +50,4 @@ arr.forEach(p => {
 });
 ctx.stroke();
 
-console.log(arr[2]);
+// console.log(util.inspect(arr, {depth: null}));
